f92f1c2ed019b003fc4801c204b367841185eb1b
Alexander Zakharchuk <mystang.pro@gmail.com>
2021-03-05 17:15:41 +0300

    Added main.py file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from __all_models import *
 from db_session import global_init, create_session
 
@@ -5,6 +6,15 @@ from db_session import global_init, create_session
 def main():
     global_init("db.db")
     session = create_session()
+    job = Jobs()
+    job.team_leader = 1
+    job.job = "deployment of residential modules 1 and 2"
+    job.work_size = 15
+    job.collaborators = "2, 3"
+    job.start_date = datetime.now()
+    job.is_finished = False
+    session.add(job)
+
     members = [
         {
             "surname": "Scott",
